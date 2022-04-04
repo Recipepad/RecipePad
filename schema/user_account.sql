708ce8ec5ce3c410c64e70e6e8024dfec718ff26
Yongzhou Gu <102498592+yg385@users.noreply.github.com>
2022-04-04 17:01:23 -0500

    Update user_account.sql

--- schema/user_account.sql
+++ schema/user_account.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `user_account` (
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `uid` int(11) unsigned NOT NULL,
+  `uid` int(11) unsigned NOT NULL DEFAULT '1000',
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
