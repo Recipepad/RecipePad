5bec84b03ed7287955a35b8d432e34aed4ecd22d
Yongzhou Gu <102498592+yg385@users.noreply.github.com>
2022-04-04 17:01:06 -0500

    Update create_database_and_tables.sql
    Change uid to auto increment

--- schema/create_database_and_tables.sql
+++ schema/create_database_and_tables.sql
@@ -2,7 +2,7 @@ CREATE DATABASE `recipepad`;
 CREATE TABLE `user_account` (
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `uid` int(11) unsigned NOT NULL,
+  `uid` int(11) unsigned NOT NULL DEFAULT '1000',
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 CREATE TABLE `user_profile` (
