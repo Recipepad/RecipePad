a8369158fff261a90ae0189d9e46675a12d2e14c
yg385 <yg7@illinois.edu>
2022-04-04 17:45:13 -0500

    Fix schema

--- schema/create_database_and_tables.sql
+++ schema/create_database_and_tables.sql
@@ -2,8 +2,8 @@ CREATE DATABASE `recipepad`;
 CREATE TABLE `user_account` (
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `uid` int(11) unsigned NOT NULL DEFAULT '1000',
-  PRIMARY KEY (`username`)
+  `uid` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`uid`,`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 CREATE TABLE `user_profile` (
   `uid` int(11) unsigned NOT NULL,
--- schema/user_account.sql
+++ schema/user_account.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `user_account` (
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `uid` int(11) unsigned NOT NULL DEFAULT '1000',
-  PRIMARY KEY (`username`)
+  `uid` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`uid`,`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
