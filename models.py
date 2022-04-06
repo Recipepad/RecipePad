c4bb64d63097dfa4e142dd76a7b5ecbac4881aa6
yg385 <yg7@illinois.edu>
2022-04-06 15:26:38 -0500

    In progress, simple recipe route

--- models.py
+++ models.py
@@ -2,8 +2,24 @@ from app import db
 
 
 class UserAccount(db.Model):
-    username = db.Column(db.String(20), primary_key=True, unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(20), unique=True, nullable=False)
+    uid = db.Column(db.Integer, primary_key=True)
 
     def __repr__(self):
         return f"username: {self.username}, password: {self.password}"
+
+
+class Recipe(db.Model):
+    rid = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String(45))
+    cover_imgid = db.Column(db.Integer)
+    description = db.Column(db.String(300))
+    ingredients = db.Column(db.JSON)
+    steps = db.Column(db.JSON)
+    tags = db.Column(db.JSON)
+
+    def __repr__(self):
+        return f"rid: {self.rid}, title: {self.title}, cover_imgid: {self.cover_imgid}, " \
+               f"description: {self.description}, ingredients: {self.ingredients}, steps: {self.steps}, " \
+               f"tags: {self.tags}"
