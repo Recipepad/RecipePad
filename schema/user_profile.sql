d4b29c281639df94a5709177c642cc586623f6bb
Yutao Sun <ys588@cornell.edu>
2022-04-22 17:48:54 -0400

    fix avatar_imgid type in database

--- schema/user_profile.sql
+++ schema/user_profile.sql
@@ -2,6 +2,6 @@ CREATE TABLE `user_profile` (
   `uid` int(11) unsigned NOT NULL,
   `nickname` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
-  `avatar_imgid` int(11) unsigned DEFAULT NULL,
+  `avatar_imgid` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
